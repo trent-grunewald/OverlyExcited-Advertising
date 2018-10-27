c509bb880cf90b2e14da234ccc099e077a4748a6
trent-grunewald <39209726+trent-grunewald@users.noreply.github.com>
2018-10-27 16:54:52 -0700

    initial

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,62 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+// const concat = require('gulp-concat')
+// const babel = require('gulp-babel')
+// const watch = require('gulp-watch')
+const browserSync = require('browser-sync')
+const reload = browserSync.reload
+var exec = require('child_process').exec;
 
-gulp.task('sass', function (){
-  //targets any folder inside the src/scss that has scss files
-  return gulp.src('./src/scss/**/*.scss')
-  //displays any error in console log
-  .pipe(sass().on('error', sass.logError))
+gulp.task('default', ['styles', 'webpack', 'browser-sync'], () => {
+  gulp.watch('./assets/sass/**/*', ['styles'])
+  gulp.watch('./assets/js/**/*', ['webpack'])
+  gulp.watch(['./public/**/*', './public/*', '!public/js/**/.#*js', '!public/css/**/.#*css']).on('change', reload)
+})
 
-  //automatically adds the autoprefixer to converted css
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions']
-  }))
+gulp.task('styles', () => {
+  gulp.src('assets/sass/**/*.scss')
+    .pipe(
+      sass({
+        outputStyle: 'compressed'
+      })
+      .on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream())
+})
 
-  //converts all the folder files to css
-  .pipe(gulp.dest('./public/css'));
-});
+gulp.task('browser-sync', ['styles'], function () {
+  // THIS IS FOR SITUATIONS WHEN YOU HAVE ANOTHER SERVER RUNNING
+  // browserSync.init({
+  //   proxy: {
+  //     target: 'localhost:3000', // can be [virtual host, sub-directory, localhost with port]
+  //     ws: true // enables websockets
+  //   },
+  //   serveStatic: ['.', './public']
+  // })
 
+  browserSync.init({
+        server: './public',
+        notify: false,
+        open: false //change this to true if you want the broser to open automatically 
+    });
+})
 
-//defaults to first task (sass) and adds a function
-gulp.task('default', ['sass'], function(){
-  //watchest the SCSS folder for any changes and runs the 'sass' task on any changes-- resulting in automatic css conversion.
-  gulp.watch('./src/scss/**/*', ['sass'])
-});
+gulp.task('webpack', (cb) => {
+  exec('webpack', function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+})
+
+// gulp.task('webpack', shell.task([
+//   'webpack'
+// ]))
+
+// gulp.task('server', shell.task([
+//   'yarn run server'
+// ]))
